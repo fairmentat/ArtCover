ca8ce9a4624700e7777a8e9c04d108bcd7bb73ce
fairmentat <fairmentat@mail.ru>
2020-04-10 20:20:40 +0400

    App complete

---
+++ ArtCover/DetailVC.swift
@@ -0,0 +1,33 @@
+//
+//  DetailVC.swift
+//  ArtCover
+//
+//  Created by Дмитрий on 09.04.2020.
+//  Copyright © 2020 BioRobot. All rights reserved.
+//
+
+import UIKit
+
+class DetailVC: UIViewController {
+    
+    
+    @IBOutlet weak var image: UIImageView!
+    
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    
+    var trackTitle = ""
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        image.image = UIImage(named: trackTitle)
+        titleLabel.text = trackTitle
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
---
+++ ArtCover/TrackList.swift
@@ -0,0 +1,98 @@
+//
+//  TrackList.swift
+//  ArtCover
+//
+//  Created by Дмитрий on 09.04.2020.
+//  Copyright © 2020 BioRobot. All rights reserved.
+//
+
+import UIKit
+
+class TrackList: UITableViewController {
+    
+    let imageNameArray = ["Сектор Газа - Демобилизация", "КИНО - Группа крови", "Сплин - Орбит без сахара", "Мумий Тролль - Утекай", "Линда - Северный ветер"]
+    
+    
+    
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    // MARK: - Table view data source
+
+   
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return imageNameArray.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "title", for: indexPath)
+
+        cell.imageView?.image = UIImage(named: imageNameArray[indexPath.row])
+        cell.textLabel?.text = imageNameArray[indexPath.row]
+
+        return cell
+    }
+    
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "ShowDetail"{
+            if let indexPath = self.tableView.indexPathForSelectedRow{
+                let detailsVC = segue.destination as! DetailVC
+                detailsVC.trackTitle = imageNameArray[indexPath.row]
+            }
+        }
+    }
+    
+
+}
